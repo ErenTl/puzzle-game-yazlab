2d4267c51cbb0f72d7fa6398c04fe689adb0e48f
ErenTl <beneyim@gmail.com>
2023-04-07 04:54:01 +0300

    scoreboard get api was added

---
+++ pages/api/scoreboard.js
@@ -0,0 +1,50 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+// import db from '../../lib/db'
+const pool = require('../../lib/db');
+
+
+
+// export default async function getSliderAds (req,res) {
+//   try{
+//       console.log("inside user/getSliderAds");
+//       var x = await pool.query(getSliderAdsQuery);
+//       res.status(200).json(x.rows);
+//   }catch(err){
+//       res.status(500).json(err);
+//   }
+// }
+
+export default async function getScoreboard (req,res) {
+  try{
+      console.log("inside user/getScoreboard");
+      var x = await pool.query("select * from scoreboard");
+      res.status(200).json(x.rows);
+  }catch(err){
+      res.status(500).json(err);
+  }
+}
+
+const getSliderAdsQuery = `
+    SELECT
+        *, index as key, "imageLink" as image
+    FROM
+        "adSlider"
+    WHERE
+        "expireDate" > now()
+    AND
+        "index" > 0
+    ORDER BY
+        "index" ASC`;
+
+
+        // export async function getCarouselItems() {
+        //     const response = await fetch('http://localhost:3000/api/v1/user/getSliderAds');
+        //     const data = await response.json();
+        //     return data;
+        // }
+        
