d23e31884103fca28ba209c9957d0393e9dffea5
ErenTl <beneyim@gmail.com>
2023-04-07 05:24:29 +0300

    apiurl was deleted

--- data/scoreboard.js
+++ data/scoreboard.js
@@ -1,7 +1,7 @@
 // require("dotenv").config();
 
 export async function getScoreboard() {
-    const response = await fetch(process.env.API_URL+'/api/scoreboard');
+    const response = await fetch('/api/scoreboard');
     const data = await response.json();
     return data;
 }
